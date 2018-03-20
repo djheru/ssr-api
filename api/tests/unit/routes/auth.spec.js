416709f50038bce6f99750d18f76a85ffa5870ab
Philip Damra <djheru@gmail.com>
2018-03-19 23:00:43 -0400

    Fix up tests

--- api/tests/unit/routes/auth.spec.js
+++ api/tests/unit/routes/auth.spec.js
@@ -4,17 +4,21 @@ import proxyquire from 'proxyquire';
 
 describe('(Auth Routes)', () => {
   let initializeAuthRoutesProxy;
+  let expressStub;
   let appStub;
+  let routerStub;
   let passportStub;
   let passportMiddlewareStub;
   let authenticateStub;
   let authCallbackStub;
   let redirectStub;
   let logoutStub;
-  let currentUserStub;
 
   beforeEach(() => {
-    appStub = { get: sinon.stub() };
+    expressStub = { Router: sinon.stub() };
+    appStub = { use: sinon.stub() };
+    routerStub = { get: sinon.stub() };
+    expressStub.Router.returns(routerStub);
     passportStub = {
       authenticate: sinon.stub()
     };
@@ -24,18 +28,16 @@ describe('(Auth Routes)', () => {
     authCallbackStub = sinon.stub();
     redirectStub = sinon.stub();
     logoutStub = sinon.stub();
-    currentUserStub = sinon.stub();
 
     const authProxyModule = proxyquire(
       '../../../src/routes/auth',
       {
-        'passport': passportStub,
-        './handlers/auth': {
+        'express': expressStub,
+        '../handlers/auth': {
           authenticate: authenticateStub,
           authCallback: authCallbackStub,
           redirect: redirectStub,
-          logout: logoutStub,
-          currentUser: currentUserStub
+          logout: logoutStub
         }
       }
     );
@@ -46,33 +48,38 @@ describe('(Auth Routes)', () => {
     expect(initializeAuthRoutes).to.be.a('function');
   });
 
-  it('should register the route "/auth/google"', () => {
+  it('should register the route "/google"', () => {
     initializeAuthRoutesProxy(appStub);
-    expectRouteIsRegistered(appStub.get, '/auth/google');
+    expectRouteIsRegistered(routerStub.get, '/google');
   });
 
   it('should register the passport authentication middleware', () => {
     initializeAuthRoutesProxy(appStub);
     expect(authenticateStub).to.be.called;
-    expect(appStub.get).to.be.calledWith('/auth/google', authenticateStub());
+    expect(routerStub.get).to.be.calledWith('/google', authenticateStub());
   });
 
-  it('should register the route "/auth/google/callback"', () => {
+  it('should register the route "/google/callback"', () => {
     initializeAuthRoutesProxy(appStub);
-    expectRouteIsRegistered(appStub.get, '/auth/google/callback');
+    expectRouteIsRegistered(routerStub.get, '/google/callback');
   });
 
   it('should register the passport authentication middleware and redirect handler', () => {
     initializeAuthRoutesProxy(appStub);
     expect(authCallbackStub).to.be.called;
     expect(redirectStub).to.be.called;
-    expect(appStub.get).to.be.calledWith('/auth/google/callback', authCallbackStub(), redirectStub());
+    expect(routerStub.get).to.be.calledWith('/google/callback', authCallbackStub(), redirectStub());
   });
 
   it('should register the route "/logout"', () => {
     initializeAuthRoutesProxy(appStub);
-    expectRouteIsRegistered(appStub.get, '/logout');
+    expectRouteIsRegistered(routerStub.get, '/logout');
     expect(logoutStub).to.be.called;
-    expect(appStub.get).to.be.calledWith('/logout', logoutStub());
+    expect(routerStub.get).to.be.calledWith('/logout', logoutStub());
+  });
+
+  it('should mount the routes on the "/auth" endpoint', () => {
+    initializeAuthRoutesProxy(appStub);
+    expect(appStub.use).to.be.calledWith('/auth', routerStub);
   });
 });
